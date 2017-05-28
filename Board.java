e6d4fab9da1208b5c7b2c473f21461a422a934e4
Simon Stockhause <simon.stockhause@mni.thm.de>
2017-05-28 23:57:06 +0200

    updated Board.java

--- Board.java
+++ Board.java
@@ -6,11 +6,37 @@ import java.util.ArrayList;
  * Created by Simon on 28.05.2017.
  */
 public class Board implements Boardable {
+    /**-----------------------------------------------------------------------------------------------------------------
+     * Fields
+     * -- int[] boards: Representation of each player
+     *                  -> boards[0]: Player 1 : Black
+     *                  -> boards[0]: Player 1 : White
+     * -- Board parent: The Node in the upper Level of the Gametree
+     *                  -> if parent = this: Root e.g highest level
+     * -- Board[] children: all Boards which can be made by moving one possible move on this current Board
+     *------------------------------------------------------------------------------------------------------------------*/
     private int[] boards;
-    private int parent;
-    private int[] children;
+    private Board parent;
+    private Board[] children;
 
+    /**-----------------------------------------------------------------------------------------------------------------
+     * Constructor
+     * -- Boards(): Root constructor. Has no parent because being the root
+     * -- Boards(Board parent): Is a child of Board
+     *                                 -> Cannot be empty
+     *------------------------------------------------------------------------------------------------------------------*/
+    public Board(){
+        parent = this;
+    }
+    public Board(Board parent){
+        this.parent = parent;
+    }
 
+    /**-----------------------------------------------------------------------------------------------------------------
+     * Methods
+     * -- children(): returns an ArrayList<Board> which contains all children Boards from this Board
+     * -- hashCode(): returns an unique HashCode for this Board
+     *------------------------------------------------------------------------------------------------------------------*/
     public ArrayList<Board> children(){
         return null;
     }
