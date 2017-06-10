202cf43e84da785145978af74feb66871a4a2ee8
xXThermalXx <bjornfranke485@googlemail.com>
2017-06-10 23:32:50 +0200

    1. Die ersten Variablen wurden hinzugefügt 2. Die Methode flipped wurde implementiert 3. Die Methode isflipped wurde implementiert 4. Die Methode getHistory wurde implementiert 5. Die Methode isWin wurde implementiert ******Anmerkungen****** - Bei den Flip methoden fehlt noch die setFlipped Methode - isWin sollte soweit funktionieren - Ich hab erstmal die Klasse  Integer für die Move Kodierung genommen, bis dass in Slack geklärt ist - Ich hab noch einen Ordner hinzugfefügt

---
+++ Muehle/Board.java
@@ -0,0 +1,110 @@
+package Muehle;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Created by xXThermalXx on 10.06.2017.
+ */
+public class Board implements MutableBoard<Integer> {
+
+    private int[] players;
+    private int[] stones;
+    private int turn;
+    private List<Integer> history;
+    private boolean flipped;
+    private int [][] hashCodes;
+
+
+    public Board() {
+        /*
+            - Anmerkungen zu Attributen
+                - Das players-Array enthält für jeden Spieler einen 24 bit langen int-Wert.
+                - Jeder Spieler hat zu Beginn des Spieles 9 Spielsteine, welche im stones-Array gespeichert werden
+                - Die Variable turn ermöglicht es den Spieler zu ermitteln der grade am zug ist
+                - In der List history werden alle vergangen Züge gespeichert
+                - Die Variable flipped speichert, ob der Benutzer die Spielsteine vertauschen möchte
+         */
+        players = new int[]{0b0, 0b0};
+        stones = new int[]{9, 9};
+        turn = 0;
+        history = new ArrayList<>();
+        flipped = false;
+    }//Constructor
+
+    @Override
+    public void makeMove(Integer integer) {
+        //TO-DO
+    }
+
+    @Override
+    public void undoMove() {
+        //TO-DO
+    }
+
+    @Override
+    public List<Integer> moves() {
+        //TO-DO
+        return null;
+    }
+
+    @Override
+    public List<Integer> getHistory() {
+        return history;
+    }//getHistory
+
+    @Override
+    public boolean isWin() {
+        if (stones[turn & 1] == 0) {
+            int winSum = IntStream.range(0, 24).filter(i -> (players[(turn & 1)] & (1 << i)) > 0).toArray().length;
+            return (winSum<3) ? true : false;
+        }//if
+        return false;
+    }
+
+    @Override
+    public boolean isDraw() {
+        //TO-DO
+        return false;
+    }
+
+    @Override
+    public void flip() {
+        if (isFlipped() == true) {
+            int dummy = players[0];
+            players[0] = players[1];
+            players[1] = dummy;
+            dummy = stones[0];
+            stones[0] = stones[1];
+            stones[1] = dummy;
+        }//nur wenn der Benutzer die Steine switchen will
+    }//flip
+
+    @Override
+    public boolean isFlipped() {
+        if (flipped == true) return true;
+        return false;
+    }
+
+    @Override
+    public void load(String name) {
+        //TO-DO
+    }
+
+    @Override
+    public void load(Path path) {
+        //TO-DO
+    }
+
+    @Override
+    public void save(String name) {
+        //TO-DO
+    }
+
+    @Override
+    public void save(Path path) {
+        //TO-DO
+    }
+}//class
