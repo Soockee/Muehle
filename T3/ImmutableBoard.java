27dee0a1b495b08f2a9c8bcf48e384a73b0ee8db
Paul Krappatsch <paul.krappatsch@mni.thm.de>
2017-06-14 22:33:59 +0200

    add Interface for T3

---
+++ T3/ImmutableBoard.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+/**
+ * Created by Paul Krappatsch on 11.06.2017.
+ */
+public interface ImmutableBoard<Move>  {
+
+    ImmutableBoard<Move> makeMove(Move move);
+
+    default ImmutableBoard<Move> makeMove(Move... moves) {
+        ImmutableBoard<Move> res = this;
+        for(Move move : moves) res = res.makeMove(move);
+        return res;
+    }
+
+    ImmutableBoard<Move> undoMove();
+
+    List<Move> moves();
+    List<Move> getHistory();
+
+    boolean isWin();
+    boolean isDraw();
+
+    default boolean isBeginnersTurn() {
+        return getHistory().size() % 2 == 0;
+    }
+
+    ImmutableBoard<Move> flip();
+    boolean isFlipped();
+    String toString();
+}
